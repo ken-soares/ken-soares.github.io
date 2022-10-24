b81a671f945b92d2c1c502a51662079d456057ec
Kenneth Soares <kensoares@gmail.com>
2022-10-24 17:28:51 +0200

    moved old website

---
+++ misc/cards/scripts/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var x = document.getElementById("myNav");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
