eb7619648a1a99f04d7dd9155c19d750c272d6b7
Kenneth Soares <kensoares@gmail.com>
2022-10-26 15:19:34 +0200

    testing animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
+const animationSpinning = [
+    {transform: 'rotate(0) scale(1)'},
+    {transform: 'rotate(360deg) scale(0)'}
+];
+
+const animationTiming = {
+    duration: 2000,
+    iterations: 1
+}
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+    x.animate()
   }
 }
